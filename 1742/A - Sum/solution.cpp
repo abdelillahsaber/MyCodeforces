8756f8892b7864e1188861b1b18c4ce91783de94
Abdelillah <abdelillahsaber1@gmail.com>
2026-07-25 11:16:03 +0100

    Add Sum [A] from Codeforces

---
+++ 1742/A - Sum/solution.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+ 
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a,b,c;
+        cin >> a >> b >> c;
+        if (a+b==c || abs(a-b)==c)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
