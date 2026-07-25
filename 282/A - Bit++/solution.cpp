f22c440b7065c9b6422b6234899ed34193df8548
Abdelillah <abdelillahsaber1@gmail.com>
2026-07-25 11:16:23 +0100

    Add Bit++ [A] from Codeforces

---
+++ 282/A - Bit++/solution.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+    int x = 0;
+    while (n--) {
+        string statement;
+        cin >> statement;
+        if (statement[1] == '+') {
+            x++;
+        } else {
+            x--;
+        }
+    }
+    cout << x << endl;
+    return 0;
+}
